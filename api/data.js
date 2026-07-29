362004ba407ce6b92f77bf83ba0ffdb59ae5e473
TheBusiness13 <ooangri@gmail.com>
2026-07-29 19:33:42 +0100

    Add files via upload

--- api/data.js
+++ api/data.js
@@ -10,8 +10,8 @@ async function readAll(workspaceId){
     sf(`ml_campaigns?workspace_id=eq.${wid}&select=*&order=created_at.asc`),
     sf(`ml_leads?workspace_id=eq.${wid}&select=*&order=created_at.asc`),
     sf(`ml_campaign_sequences?workspace_id=eq.${wid}&select=*`),
-    sf(`ml_send_jobs?workspace_id=eq.${wid}&select=*&order=created_at.desc&limit=100`),
-    sf(`ml_send_job_items?workspace_id=eq.${wid}&select=*&order=id.asc&limit=5000`),
+    sf(`ml_send_jobs?workspace_id=eq.${wid}&select=*&order=created_at.desc&limit=100`).catch(()=>[]),
+    sf(`ml_send_job_items?workspace_id=eq.${wid}&select=*&order=id.asc&limit=5000`).catch(()=>[]),
     sf(`ml_sender_profiles?workspace_id=eq.${wid}&select=*&limit=1`),
     sf(`ml_provider_profiles?workspace_id=eq.${wid}&select=*`).catch(()=>[])
   ]);
